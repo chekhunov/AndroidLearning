5b8a70f33964122e21b2717a2f9f1bbcfe4c36d4
chekhunov <scdesignk@gmail.com>
2020-02-12 23:27:47 +0200

    two lesson

---
+++ P0102_ActivityListener/src/main/java/com/example/p0102_activitylistener/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.p0102_activitylistener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+
+    Button btnStart;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // найдем View-элементы
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+        btnStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // по id определеяем кнопку, вызвавшую этот обработчик
+        switch (v.getId()) {
+            case R.id.btnOk:
+                // кнопка ОК
+                tvOut.setText("Нажата кнопка ОК");
+                break;
+            case R.id.btnCancel:
+                // кнопка Cancel
+                tvOut.setText("Нажата кнопка Cancel");
+                break;
+            case R.id.btnStart:
+                // button Start
+                tvOut.setText("Нажата кнопка Start");
+        }
+    }
+}
+
